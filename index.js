d888ada873fd8d4c016cfd487f19ab87d467ab3b
Derivaldo Brito <derivaldo.brito@yahoo.com.br>
2022-10-14 16:47:24 -0300

    Repetição de código, adicionando comportamentos

---
+++ index.js
@@ -0,0 +1,44 @@
+class Cliente {
+    nome;
+    cpf;
+}
+class ContaCorrente {
+    agencia;
+    // #saldo = 0 proposta de mudança para cerquilha ainda não implementada
+    _saldo = 0; //boa pratica usar o _ underline ao invés da cerquilha #
+
+    sacar(valor) {
+        if (this._saldo >= valor) {
+            this._saldo -= valor;
+            return valor;
+        }
+    }
+    depositar(valor) {
+        if (valor <= 0)
+        {
+           return; //o que eu não quero primeiro 
+        } 
+        this._saldo += valor;
+    }
+}
+
+const cliente1 = new Cliente();
+cliente1.nome = "Ricardo";
+cliente1.cpf = "11122233309";
+
+const cliente2 = new Cliente();
+cliente2.nome = "Alice";
+cliente2.cpf = "88822233309";
+
+const contaCorrenteRicardo = new ContaCorrente();
+
+contaCorrenteRicardo.agencia = 1001;
+
+contaCorrenteRicardo.depositar(-100);
+contaCorrenteRicardo.depositar(100);
+contaCorrenteRicardo.depositar(100);
+
+const valorSacado = contaCorrenteRicardo.sacar(50);
+console.log(valorSacado);
+
+console.log(contaCorrenteRicardo);
