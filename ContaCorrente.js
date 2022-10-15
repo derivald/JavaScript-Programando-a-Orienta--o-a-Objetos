2e38ffe7efe1f33e8c00ebec2dcdfb03897a2e12
Derivaldo Brito <derivaldo.brito@yahoo.com.br>
2022-10-15 13:07:50 -0300

    Acessando atributos privados

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,9 +1,25 @@
+import { Cliente } from "./Cliente.js";
+
 export class ContaCorrente {
     agencia;
-    cliente;
+    _cliente;
     // #saldo = 0 proposta de mudança para cerquilha ainda não implementada
 
+    set cliente(novoValor){/**Assessor set */
+        if(novoValor instanceof Cliente){
+            this._cliente = novoValor;
+        }
+    }
+    get cliente(){/**assessor get */
+        return this._cliente;
+        }
+    
+
     _saldo = 0; //boa pratica usar o _ underline ao invés da cerquilha para atributo privado
+
+    get saldo(){
+        return this._saldo;
+    }
 
     sacar(valor) {
         if (this._saldo >= valor) {
